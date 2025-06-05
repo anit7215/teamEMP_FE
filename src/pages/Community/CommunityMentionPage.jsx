20d3554ec0c64fcb83a9049cf6b133bcd21b4d06
ChaeEunCha <cherrycha03@gmail.com>
2025-06-06 00:27:16 +0900

    feat: 커뮤니티 페이지 전체 완성(추가 페이지 반영 완료)

--- src/pages/Community/CommunityMentionPage.jsx
+++ src/pages/Community/CommunityMentionPage.jsx
@@ -1,70 +1,98 @@
-import React from 'react';
+import React, { useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import Card from '../../components/common/Card/Card';
 import * as S from './Style';
 
+const CommunityMentionPage = () => {
+  const navigate = useNavigate();
+  const [mention, setMention] = useState('');
 
-const CommunityPage = () => {
-    const navigate = useNavigate();
-
-    const posts = [
+  const posts = [
     {
       id: 1,
       author: '익명1',
-      date: '2025-04-15 23:06',
+      date: '2025-04-15',
+      time: '23:06',
       content: '진짜 땡깡이라는 말 없어졌으면.. 발작 이런 것도요. 너무 이미지가 나쁜 듯해요.',
     }
-    ];
+  ];
 
-    const mentions = [
-        {
-          id: 2,
-          author: '익명2',
-          date: '2025-04-15 23:45',
-          content: '맞아요 너무 속상해요'
-        },
-        {
-          id: 2,
-          author: '익명2',
-          date: '2025-04-15 23:45',
-          content: '맞아요 너무 속상해요'
-        },
-        {
-          id: 2,
-          author: '익명2',
-          date: '2025-04-15 23:45',
-          content: '맞아요 너무 속상해요'
-        }
-    ];
+  const mentions = [
+    {
+      id: 2,
+      author: '익명2',
+      date: '2025-04-15',
+      time: '23:06',
+      content: '맞아요 너무 속상해요'
+    },
+    {
+      id: 3,
+      author: '익명3',
+      date: '2025-04-15',
+      time: '23:06',
+      content: '저도 공감해요'
+    }
+  ];
+
+  const handleMentionPage = (id) => {
+    navigate(`/community/mention/${id}`);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter' && mention.trim() !== '') {
+      console.log('댓글 전송:', mention); // TODO: 댓글 전송 API 호출
+      setMention(''); // 입력창 초기화
+    }
+  };
     
-    return (
-        <>
-        {posts.map((post) => (
-        <S.PostCard key={post.id} onClick={handleMentionPage}>
+  return (
+    <>
+      {posts.map((post) => (
+        <S.PostCard key={post.id} onClick={() => handleMentionPage(post.id)}>
           <S.Title>
-            {post.author}
-            {post.date}
-            {post.isMine && <span className="delete">삭제</span>}
-          </S.Title>
+            <S.LeftGroup>
+                <S.Author>{post.author}</S.Author>                    <S.Date>{post.date}</S.Date>
+                <S.Time>{post.time}</S.Time>
+            </S.LeftGroup>
+            <S.IsMine>
+                {post.isMine && <span className="delete">삭제</span>}
+            </S.IsMine>
+        </S.Title>
           <S.PostContent>{post.content}</S.PostContent>
-            <S.PostActions>
-                <S.IconButton icon="likes" count={7} />
-                <S.IconButton icon="mention" count={7} />
-            </S.PostActions>
+          <S.PostActions>
+            <S.IconButton icon="likes" count={7} />
+            <S.IconButton icon="mention" count={7} />
+          </S.PostActions>
         </S.PostCard>
       ))}
-        {mentions.map((mention) => (
-            <S.PostCard key={mention.id}>
-          <S.Title>
-            {mention.author}
-            {mention.date}
-            {mention.isMine && <span className="delete">삭제</span>}
-          </S.Title>
-          <S.PostContent>{mention.content}</S.PostContent>
+      {mentions.map((mention) => (
+        <S.PostCard key={mention.id}>
+        <S.Title>
+            <S.LeftGroup>
+                <S.Author>{mention.author}</S.Author>
+                <S.Date>{mention.date}</S.Date>
+                <S.Time>{mention.time}</S.Time>
+            </S.LeftGroup>
+            <S.IsMine>
+                {mention.isMine && <span className="delete">삭제</span>}
+            </S.IsMine>
+        </S.Title>
+        <S.PostContent>{mention.content}</S.PostContent>
         </S.PostCard>
-        ))}
-        </>
-    );
+      ))}
 
-}
-export default CommunityPage;
+      {/* 댓글 입력창 */}
+      <S.MentionInputWrapper>
+        <S.MentionInput
+          type="text"
+          placeholder="댓글을 입력하세요"
+          value={mention}
+          onChange={(e) => setMention(e.target.value)}
+          onKeyDown={handleKeyDown}
+        />
+      </S.MentionInputWrapper>
+    </>
+  );
+};
+
+export default CommunityMentionPage;
--- src/pages/Community/Style.js
+++ src/pages/Community/Style.js
@@ -3,15 +3,59 @@ import likesIcon from '../../assets/icons/likes.png';
 import mentionIcon from '../../assets/icons/mention.png';
 
 export const Title = styled.div`
+  display: flex;
   color: #474a52;
   font-family: Pretendard;
   font-size: 16px;
   font-style: normal;
   font-weight: 600;
   line-height: normal;
-  white-space: nowrap
+  justify-content: space-between;
+  align-items: center;
+  white-space: nowrap;
+`;
+export const LeftGroup = styled.div`
+  display: flex;
+  gap: 4px;
+  align-items: center;
+`;
+export const Author = styled.span`
+  color: #474A52;
+  font-family: Pretendard;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
 `;
 
+export const Date = styled.span`
+  color: #474A52;
+  font-family: Pretendard;
+  font-size: 8px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+export const Time = styled.span`
+  color: #474A52;
+  font-family: Pretendard;
+  font-size: 8px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+
+export const IsMine = styled.span`
+  color: #CC2F2F;
+  font-family: Pretendard;
+  font-size: 8px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  display: flex;
+  justify-content: flex-end;
+  align-items: flex-end;
+`
 export const Content = styled.div`
   color: #686b73;
   font-family: "Pretendard-Regular";
@@ -64,9 +108,9 @@ export const IconButton = styled.div`
     background-size: cover;
     background-position: center;
     background-image: url(${({ icon }) =>
-      icon === "like"
+      icon === "likes"
         ? likesIcon
-        : icon === "comment"
+        : icon === "mention"
         ? mentionIcon
         : ''});
   }
@@ -74,4 +118,41 @@ export const IconButton = styled.div`
   &::after {
     content: '${({ count }) => count}';
   }
+`;
+
+export const MentionInputWrapper = styled.div`
+  position: fixed;
+  bottom: 118px;
+  left: 0;
+  width: calc(100% - 50px); 
+  margin-left: 25px;
+  margin-right: 25px;
+
+  display: flex;
+  padding: 10px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 16px;
+
+  box-sizing: border-box;
+  background: linear-gradient(
+    100deg,
+    rgba(115, 179, 223, 0.95) -49.53%,
+    rgba(97, 160, 212, 0.95) 24.57%,
+    rgba(118, 217, 228, 0.95) 129.21%
+  );
+  box-shadow: 0px 0px 15px 0px rgba(0, 0, 0, 0.10);
+  border-radius: 8px;
+  z-index: 1000;
+`;
+
+
+export const MentionInput = styled.input`
+  width: 100%;
+  padding: 12px 16px;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  font-size: 14px;
+  box-sizing: border-box;
 `;
